0acbc73cb915b3bb26910f7a078367959e499f17
Dobryak96 <gorokhov_olexandr@ukr.net>
2024-06-17 20:58:59 +0100

    new-10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,13 +1,116 @@
-<header class="header">
-  <nav class="nav">
-    <ul class="nav-list">
-      <li class="nav-item">
-        <a href="./page-2.html" class="nav-link">Page 2</a>
-      </li>
+import flatpickr from "flatpickr";
+import  "flatpickr/dist/themes/material_orange.css";
 
-      <li class="nav-item">
-        <a href="./page-3.html" class="nav-link">Page 3</a>
-      </li>
-    </ul>
-  </nav>
-</header>
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import imageUrl from '../img/icon-error.svg';
+
+const refs = {
+  selectInput: document.getElementById('datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysField: document.querySelector('[data-days]'),
+  hoursField: document.querySelector('[data-hours]'),
+  minutesField: document.querySelector('[data-minutes]'),
+  secondsField: document.querySelector('[data-seconds]'),
+}
+
+refs.startBtn.disabled = true;
+
+let userSelectedDate;
+    
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (userSelectedDate <= currentDate) {
+      iziToast.error({
+        title: 'Error!',
+        titleColor: '#fff', 
+        message: 'Please choose a date in the future!',
+        messageSize: '16',
+        messageColor: '#fff',        
+        backgroundColor: '#ef4040',
+        imageWidth: 302,
+        position: 'topRight',
+        theme: 'dark',
+        close: true,
+        closeOnEscape: true,
+        closeOnClick: true,
+        progressBar: true,
+        progressBarColor: '#b51b1b',
+        transitionIn: 'fadeInDown',
+        transitionOut: 'fadeOutUp',
+        iconUrl: imageUrl,
+        iconColor: '#fafafb',
+      });
+      console.log('Wrong Date! \nPlease choose a date in the future!');
+      refs.startBtn.disabled = true;
+    } else {
+      refs.startBtn.disabled = false;
+    };
+  },
+};
+
+flatpickr(refs.selectInput, options);
+
+let timeLeft;
+refs.startBtn.addEventListener('click', () => {
+    refs.startBtn.disabled = true;
+    refs.selectInput.disabled = true;
+    timeLeft = setInterval(convertMs, 1000);
+});
+
+function convertMs(ms) {
+  const endTime = userSelectedDate.getTime();
+  const currentTime = Date.now();
+  ms = endTime - currentTime;
+  if (ms < 0) {
+    clearInterval(timeLeft);
+    console.log('Time Is Up!');
+    iziToast.info({
+      title: 'Hey!',
+      titleColor: '#fff',
+      message: 'Time Is Up!',
+      messageSize: '16',
+      messageColor: '#fff',
+      backgroundColor: 'green',
+      position: 'center',
+      theme: 'dark',
+      close: false,
+      closeOnEscape: true,
+      closeOnClick: true,
+      transitionIn: 'bounceInLeft',
+      transitionOut: 'fadeOutLeft',
+    });
+    refs.selectInput.disabled = false;
+    return;
+    };
+
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  };
+
+  refs.daysField.textContent = addLeadingZero(days);
+  refs.hoursField.textContent = addLeadingZero(hours);
+  refs.minutesField.textContent = addLeadingZero(minutes);
+  refs.secondsField.textContent = addLeadingZero(seconds);
+}
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,46 +1,81 @@
-<section class="vite-promo">
-  <div class="container">
-    <div class="wrapper">
-      <h2 class="title">
-        <span class="gradient">Vite</span>
-      </h2>
-      <p class="text">Next Generation Frontend Tooling</p>
-      <p class="tagline">
-        Get ready for a development environment that can finally catch up with
-        you.
-      </p>
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import imageUrlError from '../img/icon-error.svg';
+import imageUrlSuccess from '../img/icon-success.svg';
+import imageUrlAttention from '../img/icon-attention.svg';
 
-      <ul class="actions">
-        <li class="action">
-          <a
-            class="link"
-            href="https://vitejs.dev/guide/"
-            target="_blank"
-            rel="noopener noreferrer"
-            >Get Started</a
-          >
-        </li>
-        <li class="action">
-          <a
-            class="link"
-            href="https://github.com/vitejs/vite"
-            target="_blank"
-            rel="noopener noreferrer"
-            >View on GitHub</a
-          >
-        </li>
-      </ul>
-    </div>
+const form = document.querySelector('.form');
 
-    <div class="thumb">
-      <!-- Path to images as from index.html file -->
-      <img
-        class="pic"
-        src="./img/vite-logo.png"
-        alt="Vite logo"
-        width="640"
-        height="640"
-      />
-    </div>
-  </div>
-</section>
+form.addEventListener('submit', createPromise);
+
+function createPromise(event) {
+    event.preventDefault();
+
+    const delay = event.target.elements.delay.value;
+    const state = event.target.elements.state.value;
+
+    if (!delay || !state) {
+        iziToast.warning({
+            title: 'Attention!',
+            titleSize: '16',
+            titleColor: 'red',
+            message: 'You need to set Delay or State!',
+            messageSize: '16',
+            messageColor: 'red',
+            backgroundColor: 'yellow',
+            position: 'topRight',
+            iconUrl: imageUrlAttention,
+        });
+        console.log('⚠️ Attention: Field delay or state is empty!');
+        return;
+    }
+
+    new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            state === 'fulfilled' ? resolve(delay) : reject(delay);
+        }, delay);
+    })        
+    .then((result) => {
+        iziToast.success({
+                title: 'OK',
+                titleSize: '16',
+                titleColor: '#fff', 
+                message: `Fulfilled promise in ${result}ms`,
+                messageSize: '16',
+                messageColor: '#fff',        
+                backgroundColor: '#59a10d',
+                position: 'topRight',
+                theme: 'dark',
+                close: true,
+                closeOnEscape: true,
+                closeOnClick: true,
+                progressBar: true,
+                progressBarColor: '#326101',
+                iconUrl: imageUrlSuccess,
+                iconColor: '#fff',
+        });
+        console.log(`✅ Fulfilled promise in ${result}ms`);
+    })
+        
+    .catch((error) => {
+        iziToast.error({
+                title: 'Error!',
+                titleSize: '16',
+                titleColor: '#fff', 
+                message: `Rejected promise in ${error}ms`,
+                messageSize: '16',
+                messageColor: '#fff',        
+                backgroundColor: '#ef4040',
+                position: 'topRight',
+                theme: 'dark',
+                close: true,
+                closeOnEscape: true,
+                closeOnClick: true,
+                progressBar: true,
+                progressBarColor: '#ffbebe',
+                iconUrl: imageUrlError,
+                iconColor: '#fff',
+        });
+        console.log(`❌ Rejected promise in ${error}ms`);
+    });
+};
